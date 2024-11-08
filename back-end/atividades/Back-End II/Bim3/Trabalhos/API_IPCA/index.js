79a53719fd566cee4f3b5ed8dad2d32af7748797
JoaoGuerini <joaopedrogp83@gmail.com>
2024-11-08 19:19:59 -0300

    com alguns erros, mas está quase

--- back-end/atividades/Back-End II/Bim3/Trabalhos/API_IPCA/index.js
+++ back-end/atividades/Back-End II/Bim3/Trabalhos/API_IPCA/index.js
@@ -1,16 +1,36 @@
-import { returnAll, returnYears } from "./servicos/servicos.js";
+import { returnAll, returnYears, returnId, returnCalc } from "./servicos/servicos.js";
 import express from "express";
 
 const app = express()
 
 app.get('/historicoIPCA', (req, res) =>{
-    const dados = returnAll()
-    const inputYear = parseInt(req.query.ano)
+    // const dados = returnAll()
+    const inputYear = req.query.ano
 
     const dadosAno = returnYears(inputYear)
     res.json(dadosAno)
-    res.json(dados)
+    // res.json(dados)
+    
+
 })
+
+app.get('/historicoIPCA/calculo', (req, res) => {
+    const valor = parseInt(req.query.valor)
+    const mesInicial = parseInt(req.query.mesInicial)
+    const anoInicial = parseInt(req.query.anoInicial)
+    const mesFinal = parseInt(req.query.mesFinal)
+    const anoFinal = parseInt(req.query.anoFinal)
+
+    res.json(returnCalc(valor, mesInicial, anoInicial, mesFinal, anoFinal))
+})
+
+app.get('/historicoIPCA/:id', (req, res) => {
+    const id = req.params.id
+    res.json(returnId(id))
+})
+
+
+
 
 
 app.listen(8080, () => {
--- back-end/atividades/Back-End II/Bim3/Trabalhos/API_IPCA/servicos/servicos.js
+++ back-end/atividades/Back-End II/Bim3/Trabalhos/API_IPCA/servicos/servicos.js
@@ -1,24 +1,50 @@
+import { json } from 'express'
 import historicoInflacao from '../dados/dados.js'
 
 export const returnAll = () => {
     return historicoInflacao
 }
 
-export const returnYears = (yearInput) =>{
-    console.log(yearInput)
-    const dadosAno = []
+export const returnYears = (yearInput) => {
+    parseInt(yearInput, 10)
 
-    if(!yearInput || yearInput <=2014 || yearInput >= 2025) {
+    if(yearInput <=2014 || yearInput >= 2025 || isNaN(yearInput)) {
+        console.log('tipo' + typeof(yearInput) + 'sdsa' + typeof(Number))
         return 'Dados não encontrados'
     }
 
     else{
-        historicoInflacao.forEach(yearInput => {
-            if(yearInput.ano === yearInput)
-            {
-                dadosAno.push(yearInput)
-            }
-        })
-        return dadosAno
+        const allForYear = historicoInflacao.filter(year => (year.ano == yearInput))
+        return (allForYear)
     }
 }
+
+export const returnId = (inputId) => {
+    parseInt(inputId, 10)
+
+    if(isNaN(inputId)){
+        return 'Dados não encontrados'
+    }
+    else{
+        return historicoInflacao.find(id => id.id == inputId)
+    }
+
+}
+
+export const returnCalc = (valor, mesInicial, anoInicial, mesFinal, anoFinal) => {
+    let resultado = valor;
+    let initValue = historicoInflacao.find(item => item.ano === anoInicial && item.mes === mesInicial);
+
+    while (initValue.ano < anoFinal || (initValue.ano === anoFinal && initValue.mes <= mesFinal)) {
+        console.log(initValue)
+        resultado *= (1 + initValue.ipca / 100); // é  a mesmo coisa que: resultado = valor * ((1 + (ipca1/100)) * (1 + (ipca2/100)) * (1 + (ipcaN/100)))
+        initValue.mes += 1;
+
+        if (initValue.mes > 12) {
+            initValue.mes = 1;
+            initValue.ano += 1;
+        }
+    }
+
+    return `$ resultado: ${resultado.toFixed(2)}`;
+};
