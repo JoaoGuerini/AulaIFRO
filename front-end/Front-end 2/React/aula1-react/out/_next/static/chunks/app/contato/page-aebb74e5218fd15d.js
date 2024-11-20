aba96453a10234918d8059575d3d70791d1b5634
JoaoGuerini <joaopedrogp83@gmail.com>
2024-11-20 19:49:13 -0300

    aa2

---
+++ front-end/Front-end 2/React/aula1-react/out/_next/static/chunks/app/contato/page-aebb74e5218fd15d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[243],{4155:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1711,23))},1711:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[739,130,215,744],()=>s(4155)),_N_E=e.O()}]);
