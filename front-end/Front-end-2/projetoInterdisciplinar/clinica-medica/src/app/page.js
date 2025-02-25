68334d39c1c7237252dac846a82c858ca301afb6
JoaoGuerini <joaopedrogp83@gmail.com>
2025-02-25 20:36:37 -0300

    alteracoes cansado d+ quero dormir

--- front-end/Front-end-2/projetoInterdisciplinar/clinica-medica/src/app/page.js
+++ front-end/Front-end-2/projetoInterdisciplinar/clinica-medica/src/app/page.js
@@ -4,8 +4,13 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Teste</h1>
+    <div className={styles.divPrincipal}>
+      <h1 className={styles.TituloPage}>Bem-vindo à Clínica Autista</h1>
+      <p className={styles.Text}>
+        Nossa equipe de profissionais altamente qualificados está pronta para oferecer a você um atendimento médico de excelência, 
+        com dedicação e compromisso com a sua saúde. Na Clínica Autista, unimos tecnologia de ponta, atendimento humanizado e uma ampla 
+        variedade de especialidades para proporcionar o melhor cuidado e bem-estar para você e sua família.
+      </p>
     </div>
   );
 }
